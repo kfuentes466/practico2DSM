e9440335bae7b82f07a77a0a6c42a45fea327593
kfuentes466 <36283211+kfuentes466@users.noreply.github.com>
2021-04-06 00:48:37 -0600

    Animación aplicada al splash y ttransiciones hacia pantalla login.

--- TheSalon/app/src/main/java/sv/edu/udb/thesalon/MainActivity.java
+++ TheSalon/app/src/main/java/sv/edu/udb/thesalon/MainActivity.java
@@ -2,13 +2,56 @@ package sv.edu.udb.thesalon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+import android.widget.ImageView;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txtvAppName, txtvSlogan;
+    ImageView imgvLogo;
+
+    Animation bottomAnim;
+    private static  int SPLASH_SCREEN = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtvAppName = findViewById(R.id.txtvAppName);
+        txtvSlogan = findViewById(R.id.txtvSlogan);
+        imgvLogo = findViewById(R.id.imgvLogo);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        txtvSlogan.setAnimation(bottomAnim);
+        txtvAppName.setAnimation(bottomAnim);
+        imgvLogo.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), login.class);
+                Pair[] pairs = new Pair[2];
+                pairs[0] = new Pair<View, String>(imgvLogo, "logo_transition");
+                pairs[1] = new Pair<View, String>(txtvAppName, "appName_transition");
+
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                    ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this, pairs);
+                    startActivity(intent, options.toBundle());
+                    finish();
+                }else{
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        }, SPLASH_SCREEN);
     }
 }
